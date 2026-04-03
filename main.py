d6377bbfa419637cbb1629d9ad2d7c49371487e9
Igor Reis <igor.a.c.reis7@gmail.com>
2026-04-03 21:24:40 +0100

    fixed print current time

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ def load_industries():
         print(f"Error with industries file: {e}")
 
 def urlcrawer_engine(REGIONS,INDUSTRIES):
+    t = time.localtime()
     dic = []
     seen_urls = set()
     url_round = []
@@ -59,7 +60,7 @@ def urlcrawer_engine(REGIONS,INDUSTRIES):
                 ------------------------------------------------------------
                 REGION:    {region.upper():<25}
                 INDUSTRY:  {industry[0].upper():<25}
-                START TIME: {str(time.strftime("%d/%m/%Y %H:%M:%S",time.localtime()))} UPTIME: {str(timedelta(seconds=int(time.time() - START_TIME))):<25}
+                START TIME: {str(time.strftime("%d/%m/%Y %H:%M:%S",t))} UPTIME: {str(timedelta(seconds=int(time.time() - START_TIME))):<25}
                 ============================================================
                 """)
             u = URL_CRAWL + industry[0] + "/"+ region.lower().replace(" ","-")
@@ -118,8 +119,8 @@ def urlcrawer_engine(REGIONS,INDUSTRIES):
                 except Exception as e:
                     print(f"CRITICAL ERROR: {e}")
                     break
-            #save_to_db(dic)
-            save_to_excel(dic)
+            save_to_db(dic)
+            #save_to_excel(dic)
             dic=[]
     
 
